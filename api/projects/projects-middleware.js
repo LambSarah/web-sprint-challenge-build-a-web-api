039e70d15ae87e6f51c262fd7d0d1293f2bacbea
Sarah Guidry <sarahbethguidry@gmail.com>
2021-07-17 11:44:12 -0500

    get projects & get projects/:id done

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,30 @@
 // add middlewares here related to projects
+const logger = (req, res, next) => {
+	let current_date_time = new Date()
+	let formatted_date_time =
+		current_date_time.getFullYear()
+		+ '-'
+		+ (current_date_time.getMonth() + 1)
+		+ '-'
+		+ current_date_time.getDate()
+		+ ' '
+		+ current_date_time.getHours()
+		+ ':'
+		+ current_date_time.getMinutes()
+		+ ':'
+		+ current_date_time.getSeconds();
+
+	let method = req.method
+	let url = req.url
+	let host = req.headers.host
+	let resp = res.statusCode
+
+	console.log(`Method: ${method}`)
+	console.log(`Request url: ${url} from: ${host} on ${formatted_date_time}`)
+	console.log(`Response: ${resp} `)
+	next()
+}
+
+module.exports = {
+	logger
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,37 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const Projects = require('./projects-model')
+
+const router = express.Router()
+
+router.use(express.json())
+
+router.get('/', (req, res) => {
+	Projects.get()
+		.then(projects => {
+			if (projects) {
+				if (projects.length > 0) {
+					console.log('projects:', projects)
+					res.status(200).json(projects)
+				} else {
+					res.status(200).json([])
+				}
+			}
+		})
+		.catch(err => {
+			console.log(err)
+		})
+})
+
+router.get('/:id', (req, res) => {
+	Projects.get(req.params.id)
+		.then(project => {
+			project ? res.status(200).json(project) : res.status(404).json({ message: 'Project not found.' })
+		})
+		.catch(err => {
+			console.log(err)
+		})
+})
+
+module.exports = router
