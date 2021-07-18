3dbadf580c06271b80bc1b454b78b03f1c580e20
Sarah Guidry <sarahbethguidry@gmail.com>
2021-07-17 23:33:47 -0500

    get actions/:id done

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -10,9 +10,7 @@ router.use(express.json())
 
 /*
 [GET] /api/actions
-Returns an array of actions (or an empty array) as the body of the response.
- 
-*/
+Returns an array of actions (or an empty array) as the body of the response.*/
 router.get('/', (req, res) => {
 	Actions.get()
 		.then(actions => {
@@ -23,16 +21,22 @@ router.get('/', (req, res) => {
 		.catch(err => console.log(err)
 
 		)
-}
-)
+})
 
 /*
-
-
 [GET] /api/actions/:id
 Returns an action with the given id as the body of the response.
-If there is no action with the given id it responds with a status code 404.
- [POST] /api/actions
+If there is no action with the given id it responds with a status code 404.*/
+router.get('/:id', (req, res) => {
+	Actions.get(req.params.id)
+		.then(action => {
+			action ? res.status(200).json(action) : res.status(404).json({ message: 'Action does not exist' })
+		})
+		.catch(err => console.log(err))
+})
+
+/*
+[POST] /api/actions
 Returns the newly created action as the body of the response.
 If the request body is missing any of the required fields it responds with a status code 400.
 When adding an action make sure the project_id provided belongs to an existing project.
